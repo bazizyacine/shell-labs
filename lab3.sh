cde300eac35188525f076060b97349ca486f05c1
bazizyacine <58179990+bazizyacine@users.noreply.github.com>
2019-12-11 23:56:38 +0100

    test

--- lab3.sh
+++ lab3.sh
@@ -6,5 +6,6 @@ find / -type f -inum 128 2>/dev/null
 # 3.
 find /etc -name *.conf -type f -print -ok cp {} test \; 2> /dev/null
 # 4.
-
-
+find ./ -type f \( -ok ls -l {} \; -or -ok cat {} \; \)
+# 5.
+find /usr/bin -perm -4000 -exec ls -l {} \;
