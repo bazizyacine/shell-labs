1d5c03b462552a455a6dfe81efd38b1d3ecd0f33
bazizyacine <58179990+bazizyacine@users.noreply.github.com>
2019-12-12 15:54:20 +0100

    test

--- lab4.sh
+++ lab4.sh
@@ -2,17 +2,17 @@
 
 # 1.
 if test \( -f $1 -a -r $1 \)  
-	then echo "$1 est un fichier en lecture"
+then 
+	echo "$1 est un fichier en lecture"
 else 
 	echo "$1 n'est pas un fichier en lecutre"
 fi
 # 2.
 test -f $1 && test -r $1 && echo "$1 est un fichier en lecture" || echo "$1 n'est pas un fichier en lecutre"
 # 3.
-#!/bin/bash
-
 if [ $# -ne 2 ] 
-	then echo "commande : tri fichier1 fichier2"
+then 
+	echo "commande : tri fichier1 fichier2"
 else 
 	echo -e "Ligne sur l’entrée standard : \c"
 	
@@ -25,3 +25,28 @@ else
 	esac
 fi
 # 4.
+if [ $# -ne 3 ]
+then 
+	./dupliquer2
+elif  [ ! -e $1 -o ! -f $1 ]
+then 
+	exit 1
+elif [ ! -e $2 ]
+then 
+	echo "Le dossier n'existe pas, voulez vous le crée? (o/n)"
+	read reponse
+	
+	if [ "$reponse" = "o" ]
+	then 
+		mkdir $2
+	else 
+		exit 0
+	fi
+	
+	if [ ! -d $2  -o ! -w $2 ]
+	then 
+		exit 2
+	else
+		./copier2 $1 $2 $3
+        fi
+fi
