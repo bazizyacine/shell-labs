67dd42c841c8f27b76cc4724c61a9df53670fb3c
bazizyacine <58179990+bazizyacine@users.noreply.github.com>
2019-12-15 17:17:02 +0100

    test

--- lab7.sh
+++ lab7.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 #  1.
+sed 's;\(.*\):.*:\(.*\):.*:.*:\(.*\):.*;NOM = \1 UID = \2 REP = \3;' /etc/passwd
+# 2.
+sed 's;\(.*\):.*:\(.*\):.*:.*:\(.*\):.*;NOM = \1 UID = \2 REP = \3;' /etc/passwd > pass1
+# 3.
+cp /etc/passwd pass2
+cp /etc/passwd pass3
+sed -isave 's;\(.*\):.*:\(.*\):.*:.*:\(.*\):.*;NOM = \1 UID = \2 REP = \3;' pass2
